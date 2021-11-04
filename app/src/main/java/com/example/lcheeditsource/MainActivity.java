8889fc19c59b83e64ba79256e661dba407d23adb
netk <wlzkxm2@gmail.com>
2021-11-04 16:05:35 +0900

    처음 시작입니다

---
+++ app/src/main/java/com/example/lcheeditsource/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lcheeditsource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 밑으로 이름을 적어주세요요
+    }
