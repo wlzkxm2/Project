7b9d046219a19ceefd29cf8a192943e0e618acfb
gy920 <qkrtjd6377@naver.com>
2021-11-04 16:28:34 +0900

    박성범 추가용

--- app/src/main/java/com/example/lcheeditsource/MainActivity.java
+++ app/src/main/java/com/example/lcheeditsource/MainActivity.java
@@ -14,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
         // 밑으로 이름을 적어주세요요
         // 이지원입니다
         // 데이터 추가 했습니다
+        //박성범이다 코난이라고 부르도록
+        //ㅎㅎ호호
+
+
+
 
     }
 }
