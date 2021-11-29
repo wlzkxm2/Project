22aef35e7791494f6adc4082e84ab445ae8c2891
gy920 <qkrtjd6377@naver.com>
2021-11-30 01:02:31 +0900

    박성범 myprofile.java 미수정

---
+++ app/src/main/java/com/example/lcheeditsource/myprofile.java
@@ -0,0 +1,56 @@
+package com.example.lcheeditsource;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import android.content.Intent;
+import com.example.lcheeditsource.DataBase.UserInfo;
+import android.view.View;
+import com.example.lcheeditsource.DataBaseSetting.DataBaseDao;
+
+public class myprofile extends Activity {
+    private DataBaseDao mDataBaseDao;
+
+    Button back, ps;
+    //profilesBack, profilesSetting
+
+    TextView email, phone, shop, message ;
+    // text_userEmailView, text_userMessageView,text_userShopView
+
+    Intent Page ;
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+
+        back = (Button) findViewById(R.id.btn_profilesBack);
+        ps = (Button) findViewById(R.id.btn_profilesSetting);
+        email = (TextView) findViewById(R.id.text_userEmailView);
+        phone = (TextView) findViewById(R.id.text_userPhonecallView);
+        shop = (TextView) findViewById(R.id.text_userShopView);
+        message = (TextView) findViewById(R.id.text_userShopView);
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //뒤로가기
+                Page = new Intent(getApplicationContext(), Register.class);
+                startActivity(Page);
+            }
+        });
+
+        }
+
+    }
+
+
+}
+
