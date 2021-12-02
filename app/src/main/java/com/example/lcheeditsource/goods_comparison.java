732f8cffc734edddcbbd3682adf243a291baf242
jiuk <mujkc56@g.shingu.ac.kr>
2021-12-02 15:56:12 +0900

    상품비교

--- app/src/main/java/com/example/lcheeditsource/goods_comparison.java
+++ app/src/main/java/com/example/lcheeditsource/goods_comparison.java
@@ -28,7 +28,11 @@ public class goods_comparison extends Activity {
     private DataBaseDao mDataBaseDao;
 
     TextView VSText, ipadView, lenovoView, lowestView, GoodsExplainView, GoodsExplainView2,
-             textView, textView2, textView3, textView4, textView5, textView6;
+             textView, textView2, textView3, textView4, textView5, textView6, First_bar,
+             Second_bar, Third_bar, Fourth_bar, Fifth_bar, Sixth_bar, Seventh_bar, Eighth_bar,
+             Ninth_bar, Tenth_bar, Strengths, Strengths_one, Strengths_two, Strengths_three,
+             Strengths_four, Strengths_five, Strengths_six, Weakness, Weakness_one, Weakness_two,
+            Weakness_three, Weakness_four;
     ImageView loginLogo2, UserImage, imageView4;
     ImageButton back23;
     Button btn_FirstItemBuy, btn_SecondeItemBuy;
@@ -54,6 +58,28 @@ public class goods_comparison extends Activity {
         textView4 = (TextView) findViewById(R.id.textView4);
         textView5 = (TextView) findViewById(R.id.textView5);
         textView6 = (TextView) findViewById(R.id.textView6);
+        First_bar = (TextView) findViewById(R.id.First_bar);
+        Second_bar = (TextView) findViewById(R.id.Second_bar);
+        Third_bar = (TextView) findViewById(R.id.Third_bar);
+        Fourth_bar = (TextView) findViewById(R.id.Fourth_bar);
+        Fifth_bar = (TextView) findViewById(R.id.Fifth_bar);
+        Sixth_bar = (TextView) findViewById(R.id.Sixth_bar);
+        Seventh_bar = (TextView) findViewById(R.id.Seventh_bar);
+        Eighth_bar = (TextView) findViewById(R.id.Eighth_bar);
+        Ninth_bar = (TextView) findViewById(R.id.Ninth_bar);
+        Tenth_bar = (TextView) findViewById(R.id.Tenth_bar);
+        Strengths = (TextView) findViewById(R.id.Strengths);
+        Strengths_one = (TextView) findViewById(R.id.Strengths_one);
+        Strengths_two = (TextView) findViewById(R.id.Strengths_two);
+        Strengths_three = (TextView) findViewById(R.id.Strengths_three);
+        Strengths_four = (TextView) findViewById(R.id.Strengths_four);
+        Strengths_five = (TextView) findViewById(R.id.Strengths_five);
+        Strengths_six = (TextView) findViewById(R.id.Strengths_six);
+        Weakness = (TextView) findViewById(R.id.Weakness);
+        Weakness_one = (TextView) findViewById(R.id.Weakness_one);
+        Weakness_two = (TextView) findViewById(R.id.Weakness_two);
+        Weakness_three = (TextView) findViewById(R.id.Weakness_three);
+        Weakness_four = (TextView) findViewById(R.id.Weakness_four);
         loginLogo2 = (ImageView) findViewById(R.id.loginLogo2);
         UserImage = (ImageView) findViewById(R.id.UserImage);
         imageView4 = (ImageView) findViewById(R.id.imageView4);
